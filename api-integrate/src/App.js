68a64db0e56cacad0efe3b363c739edb7e18d809
ypyp66 <ypyp66@naver.com>
2020-10-30 01:06:46 +0900

    useContext 학습

--- api-integrate/src/App.js
+++ api-integrate/src/App.js
@@ -4,11 +4,14 @@ import Users2 from "./Users2";
 import Users3 from "./Users3";
 import Users4 from "./Users4";
 import Users5_1 from "./Users5_1";
+import { UsersProvider } from "./UsersContext";
 
 function App() {
   return (
     <>
-      <Users5_1 />
+      <UsersProvider>
+        <Users5_1 />
+      </UsersProvider>
     </>
   );
 }
